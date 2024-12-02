8e5e2e1358b9fcadadcb40da9af28572aa64fbab
Jonas Frey <dev@jonasfrey.de>
2024-12-02 18:25:35 +0100

    Day 2

--- AdventOfCode2024/main.swift
+++ AdventOfCode2024/main.swift
@@ -2,14 +2,17 @@
 
 let days: [any Day] = [
     Day1Part1(),
-    Day1Part2()
+    Day1Part2(),
+    Day2Part1(),
+    Day2Part2()
 ]
 
 for day in days {
     let name = String(describing: type(of: day))
     do {
         let testResult = try day.test()
-        print("\(name) Test result: \(testResult). Expected: \(day.expectedTestResult)")
+        let testStatus = testResult == day.expectedTestResult ? "✅" : "❌"
+        print("\(testStatus) \(name) Test result: \(testResult). Expected: \(day.expectedTestResult)")
         let result = try day.run()
         print("\(name) Result: \(result)")
     } catch {
